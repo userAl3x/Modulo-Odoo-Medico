6c835bd62a7557c85813829a7f476b5e113cdd49
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-03-07 00:42:52 +0100

    Create specialty.py
    Creamos el modelo de Especialidad

---
+++ hospital_management/models/specialty.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+# Modelo de especialidades médicas
+class HospitalSpecialty(models.Model):
+    _name = 'hospital.specialty'
+    _description = 'Especialidad Médica'
+
+    name = fields.Char(string='Nombre', required=True)
+    doctor_ids = fields.One2many('hospital.doctor', 'specialty_id', string='Doctores') 
