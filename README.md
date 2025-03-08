c3ae7649a49a4827a55f57980d6c20120b3b7a80
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-03-08 01:50:18 +0100

    Eliminación total debido a fallo total - empezar de nuevo

--- README.md
+++
@@ -1,2 +0,0 @@
-Aquí se va a implementar el modulo de Hospital Management para Odoo
-by Alex Jiménez y Pau Alcaraz 
--- addons/hospital_management/models/specialty.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields # type: ignore
-
-# Modelo de especialidades médicas
-class HospitalSpecialty(models.Model):
-    _name = 'hospital.specialty'
-    _description = 'Especialidad Médica'
-
-    name = fields.Char(string='Nombre', required=True)
-    doctor_ids = fields.One2many('hospital.doctor', 'specialty_id', string='Doctores') 
